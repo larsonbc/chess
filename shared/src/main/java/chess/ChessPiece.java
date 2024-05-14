3670ae90d4fa38dfac15bf6c62e7992793594cc5
larsonbc <bchaselarson@gmail.com>
2024-05-13 20:28:05 -0600

    Created override for clone method in ChessBoard and ChessPiece

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -9,7 +9,7 @@ import java.util.Objects;
  * Note: You can add to this class, but you may not alter
  * signature of the existing methods.
  */
-public class ChessPiece {
+public class ChessPiece implements Cloneable{
 
     private final ChessGame.TeamColor pieceColor;
     private final PieceType type;
@@ -17,6 +17,18 @@ public class ChessPiece {
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
         this.pieceColor = pieceColor;
         this.type = type;
+    }
+
+    @Override
+    public Object clone() {
+        try {
+            ChessPiece clone = (ChessPiece) super.clone();
+            // copy mutable state here, so the clone can't change the internals of the original
+
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
     }
 
     /**
