e97ee5fda24107349a5f2bd51d67c88d86aee93b
larsonbc <bchaselarson@gmail.com>
2024-05-14 21:31:21 -0600

    All ValidMovesTests passed

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -50,32 +50,60 @@ public class ChessGame {
      * startPosition
      */
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
-        ChessPiece piece = board.getPiece(startPosition);
-        Collection<ChessMove> moves1 = piece.pieceMoves(board, startPosition);
-        ArrayList<ChessMove> moves2 = new ArrayList<>();
-        ChessBoard originalBoard = (ChessBoard) board.clone();
+//        ChessPiece piece = board.getPiece(startPosition);
+//        Collection<ChessMove> moves1 = piece.pieceMoves(board, startPosition);
+//        ArrayList<ChessMove> moves2 = new ArrayList<>();
+//        ChessBoard originalBoard = (ChessBoard) board.clone();
+//
+//        //System.out.println(moves1);
+//
+//        for (ChessMove move : moves1) {
+//            //System.out.println("new move: " + move);
+//            ChessBoard boardClone = (ChessBoard) board.clone();
+//            setBoard(boardClone);
+//            board.addPiece(move.getEndPosition(), piece);
+//            //board.addPiece(move.getStartPosition(), new ChessPiece(null, null));
+//            board.addPiece(startPosition, null);
+//            //System.out.println(board.getPiece(new ChessPosition(startPosition.getRow(), startPosition.getColumn())));
+//            if (!isInCheck(piece.getTeamColor())) {
+//                moves2.add(move);
+//            }
+//            setBoard(originalBoard);
+//        }
+//        setBoard(originalBoard);
+//
+//        if (moves2.isEmpty()) {
+//            return null;
+//        } else {
+//            return moves2;
+//        }
 
-        //System.out.println(moves1);
+        if (board.getPiece(startPosition) != null) {
+            ChessPiece piece = board.getPiece(startPosition);
+            Collection<ChessMove> moves1 = piece.pieceMoves(board, startPosition);
+            ArrayList<ChessMove> moves2 = new ArrayList<>();
+            ChessBoard originalBoard = (ChessBoard) board.clone();
 
-        for (ChessMove move : moves1) {
-            //System.out.println("new move: " + move);
-            ChessBoard boardClone = (ChessBoard) board.clone();
-            setBoard(boardClone);
-            board.addPiece(move.getEndPosition(), piece);
-            //board.addPiece(move.getStartPosition(), new ChessPiece(null, null));
-            board.addPiece(startPosition, null);
-            //System.out.println(board.getPiece(new ChessPosition(startPosition.getRow(), startPosition.getColumn())));
-            if (!isInCheck(piece.getTeamColor())) {
-                moves2.add(move);
+            //System.out.println(moves1);
+
+            for (ChessMove move : moves1) {
+                //System.out.println("new move: " + move);
+                ChessBoard boardClone = (ChessBoard) board.clone();
+                setBoard(boardClone);
+                board.addPiece(move.getEndPosition(), piece);
+                //board.addPiece(move.getStartPosition(), new ChessPiece(null, null));
+                board.addPiece(startPosition, null);
+                //System.out.println(board.getPiece(new ChessPosition(startPosition.getRow(), startPosition.getColumn())));
+                if (!isInCheck(piece.getTeamColor())) {
+                    moves2.add(move);
+                }
+                setBoard(originalBoard);
             }
             setBoard(originalBoard);
-        }
-        setBoard(originalBoard);
 
-        if (moves2.isEmpty()) {
-            return null;
-        } else {
             return moves2;
+        } else {
+            return null;
         }
     }
 
