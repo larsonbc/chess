584344cd4cc241c40e27a410dbc5ee88de8f570c
larsonbc <bchaselarson@gmail.com>
2024-05-22 18:06:51 -0600

    Added main method and code to Server class to access web browser interface

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -10,6 +10,9 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.get("/hello", (req, res) -> "Hello World");
+
+        //call to createRoutes() method here
 
         Spark.awaitInitialization();
         return Spark.port();
@@ -18,5 +21,9 @@ public class Server {
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+
+    public static void main(String[] args) {
+        new Server().run(8080);
     }
 }
