e1d4fa6b0e54f577479a5a136db4c88d3f81fb09
larsonbc <bchaselarson@gmail.com>
2025-05-21 17:38:56 -0600

    Added positive and negative tests to GameServiceTest

--- server/src/test/java/service/GameServiceTest.java
+++ server/src/test/java/service/GameServiceTest.java
@@ -1,59 +1,90 @@
 package service;
 
-import dataaccess.DataAccessException;
-import dataaccess.MemoryAuthDAO;
-import dataaccess.MemoryGameDAO;
-import dataaccess.MemoryUserDAO;
+import dataaccess.*;
+import model.GameSummary;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import request.CreateGameRequest;
-import request.RegisterRequest;
+import request.*;
 import result.CreateGameResult;
+import result.JoinGameResult;
+import result.ListGamesResult;
 import result.RegisterResult;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 class GameServiceTest {
 
     private  GameService gameService;
-    private UserService userService;
     private RegisterResult testUser;
+    private UserDAO userDAO = new MemoryUserDAO();
+    private AuthDAO authDAO = new MemoryAuthDAO();
+    private GameDAO gameDAO = new MemoryGameDAO();
 
     @BeforeEach
     public void setUp() throws DataAccessException {
-//        gameService = new GameService(new MemoryAuthDAO(), new MemoryGameDAO());
-//        userService = new UserService(new MemoryUserDAO(), new MemoryAuthDAO());
-//        testUser = userService.register(new RegisterRequest("Test User", "testPassword", "testEmail@mail.com"));
+        gameService = new GameService(authDAO, gameDAO);
+        UserService userService = new UserService(userDAO, authDAO);
+        testUser = userService.register(new RegisterRequest("Test User", "testPassword", "testEmail@mail.com"));
     }
 
     @Test
     void createGamePositive() throws DataAccessException {
-//        CreateGameResult result = gameService.createGame(testUser.authToken(), new CreateGameRequest("Test Game"));
-//        int expectedID = 1;
-//        int actualID = result.gameID();
-//        assertEquals(expectedID, actualID);
+        CreateGameResult result = gameService.createGame(testUser.authToken(), new CreateGameRequest("Test Game"));
+        int expectedID = 1;
+        int actualID = result.gameID();
+        assertEquals(expectedID, actualID);
     }
 
     @Test
     void createGameNegative() {
+        DataAccessException exception = assertThrows(DataAccessException.class, () -> {
+            gameService.createGame(null, new CreateGameRequest("Test Game"));
+        });
+        assertEquals("Error: bad request", exception.getMessage());
     }
 
     @Test
-    void joinGamePositive() {
+    void joinGamePositive() throws DataAccessException {
+        CreateGameResult createGameResult = gameService.createGame(testUser.authToken(), new CreateGameRequest("Test Game"));
+        JoinGameResult actual = gameService.joinGame(testUser.authToken(), new JoinGameRequest("WHITE", 1));
+        JoinGameResult expected = new JoinGameResult();
+        assertEquals(expected, actual);
+
     }
 
     @Test
-    void joinGameNegative() {
+    void joinGameNegative() throws DataAccessException {
+        CreateGameResult createGameResult = gameService.createGame(testUser.authToken(), new CreateGameRequest("Test Game"));
+        DataAccessException exception = assertThrows(DataAccessException.class, () -> {
+            gameService.joinGame(testUser.authToken(), new JoinGameRequest("WHITE", 2));
+        });
+        assertEquals("Error: bad request", exception.getMessage());
     }
 
     @Test
-    void listGamesPositive() {
+    void listGamesPositive() throws DataAccessException {
+        CreateGameResult createGameResult = gameService.createGame(testUser.authToken(), new CreateGameRequest("Test Game Name"));
+        ListGamesResult actual = gameService.listGames(testUser.authToken(), new ListGamesRequest(testUser.authToken()));
+        ArrayList<GameSummary> gameSummaries = new ArrayList<>();
+        gameSummaries.add(new GameSummary(1, null, null, "Test Game Name"));
+        ListGamesResult expected = new ListGamesResult(gameSummaries);
+        assertEquals(expected, actual);
     }
 
     @Test
     void listGamesNegative() {
+        DataAccessException exception = assertThrows(DataAccessException.class, () -> {
+            gameService.listGames(null, null);
+        });
+        assertEquals("Error: bad request", exception.getMessage());
     }
 
     @Test
-    void clear() {
+    void clear() throws DataAccessException {
+        CreateGameResult createGameResult = gameService.createGame(testUser.authToken(), new CreateGameRequest("Test Game Name"));
+        assertDoesNotThrow(() -> {
+            gameService.clear();
+        });
     }
 }
