5a4d5d64e494074a1d609930370b68ed3e7611ab
larsonbc <bchaselarson@gmail.com>
2025-05-27 17:31:03 -0600

    Positive SQLGameDAO tests passing

--- server/src/test/java/dataaccess/SQLGameDAOTest.java
+++ server/src/test/java/dataaccess/SQLGameDAOTest.java
@@ -1,28 +1,68 @@
 package dataaccess;
 
+import model.GameData;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SQLGameDAOTest {
 
-    @Test
-    void createGame() {
+    private SQLGameDAO gameDAO;
+
+    @BeforeEach
+    public void setUp() throws DataAccessException {
+        gameDAO = new SQLGameDAO();
+        gameDAO.clear();
     }
 
     @Test
-    void getGame() {
+    void createGamePositive() {
+        assertDoesNotThrow(() -> {
+            gameDAO.createGame("Test Game Name");
+        });
     }
 
     @Test
-    void listGames() {
+    void createGameNegative() {
     }
 
     @Test
-    void updateGame() {
+    void getGamePositive() throws DataAccessException {
+        GameData newGame = gameDAO.createGame("Test Game Name");
+        GameData result = gameDAO.getGame(1);
+        assertEquals(newGame.gameName(), result.gameName());
     }
 
     @Test
-    void clear() {
+    void getGameNegative() throws DataAccessException {
+    }
+
+    @Test
+    void listGamesPositive() throws DataAccessException {
+        GameData newGame = gameDAO.createGame("Test Game Name");
+        assertNotNull(gameDAO.listGames());
+    }
+
+    @Test
+    void listGamesNegative() throws DataAccessException {
+    }
+
+    @Test
+    void updateGamePositive() throws DataAccessException {
+        GameData newGame = gameDAO.createGame("Test Game Name");
+        assertTrue(gameDAO.updateGame("WHITE", 1, "Test Name"));
+    }
+
+    @Test
+    void updateGameNegative() throws DataAccessException {
+    }
+
+    @Test
+    void clear() throws DataAccessException {
+        GameData newGame = gameDAO.createGame("Test Game Name");
+        assertDoesNotThrow(() -> {
+            gameDAO.clear();
+        });
     }
 }
